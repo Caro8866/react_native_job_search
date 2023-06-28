7730c6a82cb068086190baa71045fff94043082f
Caroline Thostrup <caro342h@stud.kea.dk>
2023-06-28 21:58:24 +0200

    search

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -4,7 +4,8 @@ import { useRouter } from "expo-router";
 import styles from "./welcome.style";
 import { icons, SIZES } from "../../../constants";
 const jobTypes = ["Full-Time", "Part-Time", "Contractor"];
-const Welcome = () => {
+
+const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
   const router = useRouter();
   const [activeJobType, setActiveJobType] = useState(`Full-Time`);
   return (
@@ -15,9 +16,9 @@ const Welcome = () => {
       </View>
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
-          <TextInput style={styles.searchInput} value="" onChange={() => {}} placeholder="What are you lloking for?" />
+          <TextInput style={styles.searchInput} value={searchTerm} onChangeText={(text) => setSearchTerm(text)} placeholder="What are you lloking for?" />
         </View>
-        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+        <TouchableOpacity style={styles.searchBtn} onPress={handleClick}>
           <Image source={icons.search} resizeMode="contain" style={styles.searchBtnImage} />
         </TouchableOpacity>
       </View>
